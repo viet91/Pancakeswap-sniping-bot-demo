198752417b8f605aaa2507c3e138420b577f12d4
Davidkolh <72505611+aviddot@users.noreply.github.com>
2021-05-06 18:53:56 +0200

    v2

--- configfile.py
+++ configfile.py
@@ -1,18 +1,28 @@
 import sys
 
-token_address='0'
+token_address=''
 decimals='18'
-bnb_amount='0'
+bnb_amount='0.0001'
 speedms='0'
-sec_wait='20'
-liquidity_decrease='0'
-max_slippage='49'
-gwei='20'
+sec_wait='1'
+max_slippage='10'
+gwei='5'
 infura_url='https://bsc-dataseed3.defibit.io/'
-enable_liq_decrease='0'
+
 enable_waiting='1'
 liq_before='1'
-approvetoken='0'
+approvetoken='1'
+
+enable_pricebuying='1'
+enable_priceselling='1'
+enablesellslippage='1'
+onlysell='0'
+
+gasusage='500000'
+sellslippage='40'
+pricebuy='2'
+pricesell='0.5'
+
 
 version='2'
 amount_threads='1'
